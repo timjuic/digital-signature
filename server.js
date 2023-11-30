2e65ef3794541bba97d207b518e19b04470e06b7
timjuic <tim.juic@gmail.com>
2023-11-30 08:44:54 +0100

    Asymmetric decryption done

--- server.js
+++ server.js
@@ -44,33 +44,71 @@ app.use(cors());
 // });
 
 
-app.post('/processFile/asymmetric', upload.single('file'), async (req, res) => {
+app.post('/asymmetricEncryption', upload.single('file'), async (req, res) => {
     try {
         if (!req.file) {
             return res.status(400).send('No file uploaded');
         }
 
         const fileContent = req.file.buffer.toString('utf-8');
-        console.log("fileContent", fileContent)
 
-        const { encryptedText, publicKey, privateKey } = await encryptAsymmetric(fileContent);
+        // Calculate the hash of the original data before encryption
+        const hash = calculateHash(fileContent);
+
+        // Encrypt the original data and its hash
+        const { encryptedText, publicKey, privateKey } = await encryptAsymmetric(fileContent + hash);
         await saveToFile('private_key.txt', privateKey);
         await saveToFile('public_key.txt', publicKey);
-
-        console.log('Encripted text', encryptedText)
         await saveToFile('asymmetric_encrypted_text.txt', encryptedText);
 
-        // Generate a digital signature
         const signature = await signText(fileContent);
-        console.log("signature", signature)
 
         // Save the digital signature to a separate file
         await saveToFile('digital_signature.txt', signature);
 
-        const hash = calculateHash(fileContent);
-        await saveToFile('hash.txt', hash)
+        // Save the hash to a separate file
+        await saveToFile('hash.txt', hash);
 
-        res.status(200).send('Asymmetric encryption with digital signature completed successfully');
+        res.status(200).send('Asymmetric encryption with digital signature and hashing completed successfully');
+    } catch (error) {
+        console.error('Error processing file:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+
+app.post('/asymmetricDecryption', upload.single('file'), async (req, res) => {
+    try {
+        if (!req.file) {
+            return res.status(400).send('No file uploaded');
+        }
+
+        const fileContent = req.file.buffer.toString('utf-8');
+
+        // Read the encrypted text from the file
+        const encryptedTextWithHash = await readFile('asymmetric_encrypted_text.txt');
+
+        // Read the public key from the file
+        const publicKey = await readFile('public_key.txt');
+
+        // Perform asymmetric decryption logic
+        const decryptedTextWithHash = decryptAsymmetric(encryptedTextWithHash, publicKey);
+
+        // Separate the decrypted data and hash
+        const decryptedData = decryptedTextWithHash.slice(0, -64); // Assuming SHA-256 hash (64 characters)
+        const decryptedHash = decryptedTextWithHash.slice(-64);    // Assuming SHA-256 hash (64 characters)
+
+        // Verify the integrity of the decrypted data
+        const isIntegrityVerified = calculateHash(decryptedData) === decryptedHash;
+
+        if (isIntegrityVerified) {
+            // Save the decrypted text to a file
+            await saveToFile('decrypted_text.txt', decryptedData);
+
+            res.status(200).send('Asymmetric decryption and signature verification completed successfully');
+        } else {
+            res.status(400).send('Digital signature verification or data integrity check failed');
+        }
     } catch (error) {
         console.error('Error processing file:', error);
         res.status(500).send('Internal Server Error');
@@ -78,28 +116,6 @@ app.post('/processFile/asymmetric', upload.single('file'), async (req, res) => {
 });
 
 // Endpoint for symmetric encryption
-app.post('/processFile/symmetric', upload.single('file'), async (req, res) => {
-    try {
-        if (!req.file) {
-            return res.status(400).send('No file uploaded');
-        }
-
-        const fileContent = req.file.buffer.toString('utf-8');
-
-        // Perform symmetric encryption logic (use a secure encryption library or algorithm)
-        // For demonstration purposes, using a simple Caesar cipher
-        console.log("CONTENT", fileContent)
-        const encryptedText = await encryptSymmetric(fileContent);
-
-        // Save the encrypted text to a file
-        await saveToFile(encryptedText, 'symmetric_encrypted_text.txt');
-
-        res.status(200).send('Symmetric encryption completed successfully');
-    } catch (error) {
-        console.error('Error processing file:', error);
-        res.status(500).send('Internal Server Error');
-    }
-});
 
 
 async function encryptAsymmetric(text) {
@@ -111,20 +127,35 @@ async function encryptAsymmetric(text) {
     });
 
     // Use the public key to encrypt the content
-    const encryptedText = crypto.publicEncrypt(publicKey, Buffer.from(text, 'utf-8')).toString('base64');
+    const encryptedText = crypto.privateEncrypt(privateKey, Buffer.from(text, 'utf-8')).toString('base64');
 
     return { encryptedText, publicKey, privateKey };
 }
 
+function decryptAsymmetric(encryptedText, publicKey) {
+    // Perform asymmetric decryption logic
+    // For demonstration purposes, using Node.js built-in crypto library
+    return crypto.publicDecrypt({
+        key: publicKey,
+        padding: crypto.constants.RSA_PKCS1_PADDING
+    }, Buffer.from(encryptedText, 'base64')).toString('utf-8');
+}
+
+function verifySignature(data, signature, publicKey) {
+    let verifier = crypto.createVerify("RSA-SHA256");
+    verifier.update(data);
+    let result = verifier.verify(publicKey, signature, "base64");
+    console.log(result)
+    return result;
+}
+
+
 async function signText(text) {
     // Use the private key to create a digital signature
-    const privateKey = crypto.generateKeyPairSync('rsa', {
-        modulusLength: 2048,
-        publicKeyEncoding: { type: 'spki', format: 'pem' },
-        privateKeyEncoding: { type: 'pkcs8', format: 'pem' }
-    }).privateKey;
-
-    const signature = crypto.sign('sha256', Buffer.from(text, 'utf-8'), { key: privateKey, padding: crypto.constants.RSA_PKCS1_PSS_PADDING });
+    const signature = crypto.sign('RSA-SHA256', Buffer.from(text, 'utf-8'), {
+        key: await readFile('private_key.txt'),
+        padding: crypto.constants.RSA_PKCS1_PADDING
+    });
 
     return signature.toString('base64');
 }
@@ -132,7 +163,7 @@ async function signText(text) {
 function calculateHash(text) {
     // Perform hash calculation logic (use a secure hashing library or algorithm)
     // For demonstration purposes, using Node.js built-in crypto library
-    const hash = crypto.createHash('sha256').update(text).digest('hex');
+    const hash = crypto.createHash('RSA-SHA256').update(text).digest('hex');
     return hash;
 }
 
@@ -141,6 +172,11 @@ async function saveToFile(fileName, content) {
     const filePath = path.join(__dirname, fileName);
     await fs.writeFile(filePath, content);
     console.log(`${fileName} saved to ${filePath}`);
+}
+
+async function readFile(fileName) {
+    const filePath = path.join(__dirname, fileName);
+    return await fs.readFile(filePath, 'utf-8');
 }
 
 app.listen(port, () => {
